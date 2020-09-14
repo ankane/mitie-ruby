c18af3002e766cfa9bb15197dd70059e35817bb5
Andrew Kane <andrew@chartkick.com>
2020-09-14 14:58:47 -0700

    Fixed mkdir on AppVeyor

--- test/ci/install_models.sh
+++ test/ci/install_models.sh
@@ -4,6 +4,8 @@ set -e
 
 CACHE_DIR=$HOME/MITIE-models/$MITIE_MODELS_VERSION
 
+mkdir $HOME/MITIE-models
+
 if [ ! -d "$CACHE_DIR" ]; then
   cd /tmp
   wget https://github.com/mit-nlp/MITIE/releases/download/v0.4/MITIE-models-v$MITIE_MODELS_VERSION.tar.bz2
