562e80991871fed73229cff88be9a2a77b4146c1
Andrew Kane <andrew@chartkick.com>
2020-09-14 22:38:56 -0700

    Cleaner variable names in tests

--- test/document_test.rb
+++ test/document_test.rb
@@ -2,43 +2,43 @@ require_relative "test_helper"
 
 class DocumentTest < Minitest::Test
   def test_entities
-    expected_entities = [
+    expected = [
       {:text=>"Nat Friedman", :tag=>"PERSON", :score=>1.099661347535191, :offset=>0, :token_index=>0, :token_length=>2},
       {:text=>"GitHub", :tag=>"ORGANIZATION", :score=>0.3446416512516501, :offset=>27, :token_index=>6, :token_length=>1},
       {:text=>"San Francisco", :tag=>"LOCATION", :score=>1.428241888939011, :offset=>61, :token_index=>12, :token_length=>2}
     ]
-    assert_equal expected_entities, doc.entities
+    assert_equal expected, doc.entities
   end
 
   def test_entities_tokens
-    expected_entities = [
+    expected = [
       {:text=>"Nat Friedman", :tag=>"PERSON", :score=>1.099661347535191, :token_index=>0, :token_length=>2},
       {:text=>"GitHub", :tag=>"ORGANIZATION", :score=>0.3446416512516501, :token_index=>6, :token_length=>1},
       {:text=>"San Francisco", :tag=>"LOCATION", :score=>1.428241888939011, :token_index=>12, :token_length=>2}
     ]
     tokens = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
-    assert_equal expected_entities, model.doc(tokens).entities
+    assert_equal expected, model.doc(tokens).entities
   end
 
   def test_tokens
-    expected_tokens = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
-    assert_equal expected_tokens, doc.tokens
+    expected = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
+    assert_equal expected, doc.tokens
   end
 
   def test_tokens_tokens
-    expected_tokens = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
-    assert_equal expected_tokens, model.doc(expected_tokens).tokens
+    expected = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
+    assert_equal expected, model.doc(expected).tokens
   end
 
   def test_tokens_with_offset
-    expected_tokens_with_offset = [["Nat", 0], ["Friedman", 4], ["is", 13], ["the", 16], ["CEO", 20], ["of", 24], ["GitHub", 27], [",", 33], ["which", 35], ["is", 41], ["headquartered", 44], ["in", 58], ["San", 61], ["Francisco", 65]]
-    assert_equal expected_tokens_with_offset, doc.tokens_with_offset
+    expected = [["Nat", 0], ["Friedman", 4], ["is", 13], ["the", 16], ["CEO", 20], ["of", 24], ["GitHub", 27], [",", 33], ["which", 35], ["is", 41], ["headquartered", 44], ["in", 58], ["San", 61], ["Francisco", 65]]
+    assert_equal expected, doc.tokens_with_offset
   end
 
   def test_tokens_with_offset_tokens
-    expected_tokens_with_offset = [["Nat", nil], ["Friedman", nil], ["is", nil], ["the", nil], ["CEO", nil], ["of", nil], ["GitHub", nil], [",", nil], ["which", nil], ["is", nil], ["headquartered", nil], ["in", nil], ["San", nil], ["Francisco", nil]]
+    expected = [["Nat", nil], ["Friedman", nil], ["is", nil], ["the", nil], ["CEO", nil], ["of", nil], ["GitHub", nil], [",", nil], ["which", nil], ["is", nil], ["headquartered", nil], ["in", nil], ["San", nil], ["Francisco", nil]]
     tokens = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
-    assert_equal expected_tokens_with_offset, model.doc(tokens).tokens_with_offset
+    assert_equal expected, model.doc(tokens).tokens_with_offset
   end
 
   def test_tokens_utf8
--- test/ner_test.rb
+++ test/ner_test.rb
@@ -2,17 +2,17 @@ require_relative "test_helper"
 
 class NERTest < Minitest::Test
   def test_entities
-    expected_entities = [
+    expected = [
       {:text=>"Nat Friedman", :tag=>"PERSON", :score=>1.099661347535191, :offset=>0, :token_index=>0, :token_length=>2},
       {:text=>"GitHub", :tag=>"ORGANIZATION", :score=>0.3446416512516501, :offset=>27, :token_index=>6, :token_length=>1},
       {:text=>"San Francisco", :tag=>"LOCATION", :score=>1.428241888939011, :offset=>61, :token_index=>12, :token_length=>2}
     ]
-    assert_equal expected_entities, model.entities(text)
+    assert_equal expected, model.entities(text)
   end
 
   def test_tokens
-    expected_tokens = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
-    assert_equal expected_tokens, model.tokens(text)
+    expected = ["Nat", "Friedman", "is", "the", "CEO", "of", "GitHub", ",", "which", "is", "headquartered", "in", "San", "Francisco"]
+    assert_equal expected, model.tokens(text)
   end
 
   def test_tokens_utf8
@@ -20,8 +20,8 @@ class NERTest < Minitest::Test
   end
 
   def test_tokens_with_offset
-    expected_tokens_with_offset = [["Nat", 0], ["Friedman", 4], ["is", 13], ["the", 16], ["CEO", 20], ["of", 24], ["GitHub", 27], [",", 33], ["which", 35], ["is", 41], ["headquartered", 44], ["in", 58], ["San", 61], ["Francisco", 65]]
-    assert_equal expected_tokens_with_offset, model.tokens_with_offset(text)
+    expected = [["Nat", 0], ["Friedman", 4], ["is", 13], ["the", 16], ["CEO", 20], ["of", 24], ["GitHub", 27], [",", 33], ["which", 35], ["is", 41], ["headquartered", 44], ["in", 58], ["San", 61], ["Francisco", 65]]
+    assert_equal expected, model.tokens_with_offset(text)
   end
 
   def test_tags
