cbe07d2e61744d66a9199b335bd433adf0bd2266
Andrew Kane <andrew@ankane.org>
2022-06-08 01:51:13 -0700

    Improved tests

--- test/mitie_test.rb
+++ test/mitie_test.rb
@@ -2,11 +2,12 @@ require_relative "test_helper"
 
 class MitieTest < Minitest::Test
   def test_tokenize
-    expected = ["Nat", "works", "at", "GitHub", "in", "San", "Francisco"]
-    assert_equal expected, Mitie.tokenize(text)
+    tokens = Mitie.tokenize(text)
+    assert_equal ["Nat", "works", "at", "GitHub", "in", "San", "Francisco"], tokens
+    assert tokens.all? { |t| t.encoding == Encoding::UTF_8 }
   end
 
-  def test_tokenize_ascii
+  def test_tokenize_encoding
     tokens = Mitie.tokenize(text.dup.force_encoding(Encoding::US_ASCII))
     assert tokens.all? { |t| t.encoding == Encoding::US_ASCII }
   end
@@ -15,9 +16,8 @@ class MitieTest < Minitest::Test
     tempfile = Tempfile.new
     tempfile.write(text)
     tempfile.flush
-    expected = ["Nat", "works", "at", "GitHub", "in", "San", "Francisco"]
     tokens = Mitie.tokenize_file(tempfile.path)
-    assert_equal expected, tokens
+    assert_equal ["Nat", "works", "at", "GitHub", "in", "San", "Francisco"], tokens
     assert tokens.all? { |t| t.encoding == Encoding::ASCII_8BIT }
   end
 end
