11c2f95ce276f0f6dae57f680ade5936d4a17984
Andrew Kane <andrew@ankane.org>
2025-04-30 12:58:30 -0700

    Added GC stress testing [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,18 @@ require "minitest/autorun"
 require "minitest/pride"
 
 class Minitest::Test
+  def setup
+    GC.stress = true if stress?
+  end
+
+  def teardown
+    GC.stress = false if stress?
+  end
+
+  def stress?
+    ENV["STRESS"]
+  end
+
   # memoize for performance
   def model
     @@model ||= Mitie::NER.new("#{models_path}/ner_model.dat")
